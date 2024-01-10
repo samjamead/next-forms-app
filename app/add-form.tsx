67df826870a4b72dd17575caddf73ceb2044c8ed
Sam Mead <samjamead@gmail.com>
2024-01-10 18:33:56 +0100

    Simplified, state only version

--- app/add-form.tsx
+++ app/add-form.tsx
@@ -1,34 +1,36 @@
-'use client'
+'use client';
 
-import { useFormState } from 'react-dom'
-import { useFormStatus } from 'react-dom'
-import { createTodo } from '@/app/actions'
+import { useFormState } from 'react-dom';
+import { useFormStatus } from 'react-dom';
+import { createTodo } from '@/app/actions';
 
 const initialState = {
-  message: '',
-}
+  messages: [],
+};
 
 function SubmitButton() {
-  const { pending } = useFormStatus()
+  const { pending } = useFormStatus();
 
   return (
-    <button type="submit" aria-disabled={pending}>
-      Add
+    <button type='submit' aria-disabled={pending}>
+      {pending ? 'Submitting...' : 'Add'}
     </button>
-  )
+  );
 }
 
 export function AddForm() {
-  const [state, formAction] = useFormState(createTodo, initialState)
+  const [state, formAction] = useFormState(createTodo, initialState);
 
   return (
     <form action={formAction}>
-      <label htmlFor="todo">Enter Task</label>
-      <input type="text" id="todo" name="todo" required />
+      <label htmlFor='todo'>Enter Task</label>
+      <input type='text' id='todo' name='todo' required />
       <SubmitButton />
-      <p aria-live="polite" className="sr-only" role="status">
-        {state?.message}
-      </p>
+      <ul role='status'>
+        {state?.messages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
     </form>
-  )
+  );
 }
